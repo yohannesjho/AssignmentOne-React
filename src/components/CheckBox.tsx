7011b40a62948833c16a6ff92de920d6ae9fbb19
yohannesjho <jhonians@outlook.com>
2025-09-06 22:55:34 +0300

    strucure and style checkbox

--- src/components/CheckBox.tsx
+++ src/components/CheckBox.tsx
@@ -1,9 +1,25 @@
- 
+import React from "react";
 
-const CheckBox = () => {
-  return (
-    <div>CheckBox</div>
-  )
+interface CheckboxProps {
+  label: string;
+  checked: boolean;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default CheckBox
+export default function Checkbox({ label, checked, onChange }: CheckboxProps) {
+  return (
+    <label className="flex justify-between items-center cursor-pointer w-full">
+      <span className="text-sm font-medium text-gray-800">{label}</span>
+      <input
+        type="checkbox"
+        checked={checked}
+        onChange={onChange}
+        className="appearance-none w-6 h-6 border border-gray-300 rounded-md checked:bg-blue-500 checked:border-blue-500 relative cursor-pointer
+          hover:border-gray-400 active:bg-blue-700
+          checked:hover:bg-blue-400 checked:active:bg-blue-700
+        before:content-[''] before:absolute before:w-[13px] before:h-[8px] before:left-[6px] before:top-[8px]
+        checked:before:bg-[url('/Vector141.svg')] checked:before:bg-contain checked:before:bg-no-repeat"
+      />
+    </label>
+  );
+}
